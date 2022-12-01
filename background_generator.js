b1f649bf2cb42f7d34bdab7bd016ca4897024239
Shubham-Arya0 <115769086+Shubham-Arya0@users.noreply.github.com>
2022-12-01 15:59:28 +0530

    Add files via upload

---
+++ background_generator.js
@@ -0,0 +1,11 @@
+var colNumber = document.querySelector("h3");
+var col1 = document.querySelector(".color1");
+var col2 = document.querySelector(".color2");
+var body = document.getElementById("gradient")
+
+function setGradient(){
+   body.style.background ="linear-gradient(to right,"+ col1.value + ","+ col2.value +")";
+   colNumber.textContent=body.style.background +";";
+}
+col1.addEventListener("input", setGradient);
+col2.addEventListener("input", setGradient);
